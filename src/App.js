02914dd73c08f16387c282fc688417be372ef7ce
annafagocistka <anafagocistka@gmail.com>
2019-01-19 23:51:02 +0100

    menu

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Menu from './Menu';
 
 class App extends Component {
   render() {
@@ -20,9 +21,11 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <Menu />
       </div>
     );
   }
 }
 
 export default App;
+
---
+++ src/Menu.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+const Menu = () => ( 
+    <Menu>
+    <Menu.Item>
+    Home 
+    </Menu.Item> 
+    <Menu.Item>
+    Form 
+    </Menu.Item> 
+    <Menu.Item>
+    Images 
+    </Menu.Item> 
+    <Menu.Item>
+    Cards 
+    </Menu.Item> 
+    </Menu >
+);
+
+export default Menu;
