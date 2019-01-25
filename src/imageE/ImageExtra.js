b8232ae5a4a863358931ff165d943aaf9ba1b68f
annafagocistka <anafagocistka@gmail.com>
2019-01-25 21:09:21 +0100

    changes trying

--- src/imageE/ImageExtra.js
+++ src/imageE/ImageExtra.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { Image } from 'semantic-ui-react';
 import jednorozec from '../img/jednorozec.jpg';
 import './ImageExtra.css';
+import Button from '../Button';
 
 
-const ImageExampleImage= (props) =>
-<Image src={jednorozec} hidden={props.hidden} size='big' className='padding' centered='true' />
+const ImageExampleImage = props =>
+  <Image centered="true" className="padding" hidden={props.hidden} size="big" src={jednorozec} />;
 
-export default ImageExampleImage
+export default ImageExampleImage;
