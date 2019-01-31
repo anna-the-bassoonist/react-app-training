dc0eb5da9eb147efeedd76e625dbbbe58e4cd4b1
annafagocistka <anafagocistka@gmail.com>
2019-01-31 01:24:26 +0100

    nav works

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,15 +6,24 @@ class Header extends React.Component {
     super(props);
     this.state = { date: new Date() };
   }
-  render() {
-    return (
-      <div>
-        <h2>Welcome</h2>
-        <h3>Now is {this.state.date.toLocaleTimeString()}</h3>
-      </div>
+  componentDidMount() {
+    this.intervalID = setInterval(
+      () => this.tick(),
+      1000,
     );
   }
+    tick = () => {
+      this.setState({
+        date: new Date(),
+      });
+    }
+    render() {
+      return (
+        <div>
+          <h2>Welcome</h2>
+          <h3>Now is {this.state.date.toLocaleTimeString()}</h3>
+        </div>
+      );
+    }
 }
-
-setInterval(1000);
 export default Header;
